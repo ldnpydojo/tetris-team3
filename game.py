e972ef26827d041a72884aea7ff198c0293dfd1d
Erdinc Mutlu <erdinc@luno.com>
2020-05-07 20:36:20 +0100

    Tiles added

--- game.py
+++ game.py
@@ -2,10 +2,19 @@ from wasabi2d import Scene, run
 
 scene = Scene()
 
-circle = scene.layers[0].add_circle(
-    radius=30,
-    pos=(scene.width / 2, scene.height / 2),
-    color='green',
+rect = scene.layers[0].add_line(
+    vertices=[(50, 50), (50, 540), (300, 540), (300, 50)],
+    color='red',
 )
+
+def grid_to_screen(x, y):
+    return x*24+66, y*24+66
+
+
+for j in range(0, 20):
+    for i in range(0, 10):
+        x, y = grid_to_screen(i, j)
+        scene.layers[1].add_sprite("tile", pos=(x, y), color='green', scale=0.75)
+
 
 run()
